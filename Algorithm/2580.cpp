2bd3d8da7cb0b7efef616b2e5f1fc7722a7573cc
Han <mgu1228@gmail.com>
2019-09-28 03:21:29 +0900

    Fuck I solved it - prob 2580.

--- Algorithm/2580.cpp
+++ Algorithm/2580.cpp
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <utility>
 using namespace std;
 
 struct coord
@@ -28,69 +29,15 @@ int countOfZero{ 0 };
 
 int tmp;
 
-void searchHorizontal();
-void searchVertical();
-void searchSquare();
-
-void doAssumeNow();
-void checkPossibility();
-
-void printSdoku();
-
-int zeroNumHor[9];
-int zeroNumVer[9];
-int zeroNumSqr[3][3];
-
-int hunt;
-
-vector<coord> v;
-stack<int> s;
+vector<coord> vCoord;
+vector<int> vVal;
+stack<pair<int, int>> s;
 
 bool odds[9];
 
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(NULL);
+int layer;
 
-	//입력 받는다
-	for (int i = 0; i < 9; ++i)
-	{
-		for (int j = 0; j < 9; ++j)
-		{
-			cin >> tmp;
-			sdoku[i][j] = tmp;
-		}
-	}
-
-	//0의 갯수를 센다
-	for (int i = 0; i < 9; ++i)
-	{
-		for (int j = 0; j < 9; ++j)
-		{
-			if (sdoku[i][j] == 0)
-			{
-				zeroNumHor[i]++;
-				zeroNumVer[j]++;
-				zeroNumSqr[i / 3][j / 3]++;
-				countOfZero++;
-			}
-		}
-	}
-
-	//일단 확정적인 것들을 구한다
-	while (!!countOfZero)
-	{
-		hunt = 0;
-		searchHorizontal();
-		searchVertical();
-		searchSquare();
-		if (hunt == 0)
-			break;
-	}
-
-	printSdoku();
-}
+bool done = false;
 
 void printSdoku()
 {
@@ -106,134 +53,72 @@ void printSdoku()
 	}
 }
 
-void searchHorizontal()
+bool check()
 {
-	int zero, sum;
+	bool ret = true;
 
-	for (int i = 0; i < 9; ++i)
-	{
-		if (zeroNumHor[i] == 1)
-		{
-			zero = 0;
-			sum = 0;
-
-			for (int j = 0; j < 9; ++j)
-			{
-				if (sdoku[i][j] == 0)
-					zero = j;
-				else
-					sum += sdoku[i][j];
-			}
-
-			sdoku[i][zero] = 45 - sum;
-
-			zeroNumHor[i]--;
-			zeroNumVer[zero]--;
-			zeroNumSqr[i / 3][zero / 3]--;
-			countOfZero--;
-			hunt++;
-		}
-	}
-}
-
-void searchVertical()
-{
-	int zero, sum;
-
-	for (int i = 0; i < 9; ++i)
-	{
-		if (zeroNumVer[i] == 1)
-		{
-			zero = 0;
-			sum = 0;
-
-			for (int j = 0; j < 9; ++j)
-			{
-				if (sdoku[j][i] == 0)
-					zero = j;
-				else
-					sum += sdoku[j][i];
-			}
-
-			sdoku[zero][i] = 45 - sum;
-
-			zeroNumHor[zero]--;
-			zeroNumVer[i]--;
-			zeroNumSqr[zero / 3][i / 3]--;
-			countOfZero--;
-			hunt++;
-		}
-	}
-}
-
-void searchSquare()
-{
 	int sum;
-	int zeroX, zeroY;
+
+	for (int i = 0; i < 9; ++i)
+	{
+		sum = 0;
+		for (int j = 0; j < 9; ++j)
+		{
+			sum += sdoku[i][j];
+		}
+		if (sum != 45)
+		{
+			ret = false;
+			return ret;
+		}
+	}
+	for (int i = 0; i < 9; ++i)
+	{
+		sum = 0;
+		for (int j = 0; j < 9; ++j)
+		{
+			sum += sdoku[j][i];
+		}
+		if (sum != 45)
+		{
+			ret = false;
+			return ret;
+		}
+	}
+
+	int XAxis, YAxis;
 
 	for (int i = 0; i < 3; ++i)
 	{
+		XAxis = 1 + i * 3;
 		for (int j = 0; j < 3; ++j)
 		{
-			if (zeroNumSqr[i][j] == 1)
+			YAxis = 1 + j * 3;
+
+			sum = 0;
+			for (int x = -1; x < 2; ++x)
 			{
-				sum = 0;
-
-				int pHor = 3*i + 1;
-				int pVer = 3*j + 1; 
-
-				for (int x = -1; x < 2; ++x)
+				for (int y = -1; y < 2; ++y)
 				{
-					for (int y = -1; y < 2; ++y)
-					{
-						if (sdoku[pHor + x][pVer + y] == 0)
-						{
-							zeroX = pHor + x;
-							zeroY = pVer + y;
-						}
-						else
-							sum += sdoku[pHor + x][pVer + y];
-					}
+					sum += sdoku[XAxis + x][YAxis + y];
 				}
-
-				sdoku[zeroX][zeroY] = 45 - sum;
-
-				zeroNumHor[zeroX]--;
-				zeroNumVer[zeroY]--;
-				zeroNumSqr[zeroX / 3][zeroY / 3]--;
-				countOfZero--;
-				hunt++;
+			}
+			if (sum != 45)
+			{
+				ret = false;
+				return ret;
 			}
 		}
 	}
+
+	return ret;
 }
 
-void doAssumeNow()
+
+void checkPossibility(int n)
 {
-	for (int i = 0; i < 9; ++i)
-	{
-		for (int j = 0; j < 9; ++j)
-		{
-			if (sdoku[i][j] == 0)
-				v.push_back({ i, j });
-		}
-	}
+	coord c = vCoord[n];
 
-	checkPossibility(v[0]);
-
-	for (int i = 0; i < 9; ++i)
-	{
-		if (odds[i])
-		{
-			s.push(i + 1);
-		}
-	}
-
-	dfs(1);
-}
-
-void checkPossibility(coord c)
-{
 	for (int i = 0; i < 9; ++i)
 	{
 		odds[i] = true;
@@ -253,26 +138,107 @@ void checkPossibility(coord c)
 	{
 		for (int y = -1; y < 2; ++y)
 		{
-			if (sdoku[1 + c.x/3 + x][1 + c.y/3 + y] != 0)
-				odds[sdoku[1 + c.x / 3 + x][1 + c.y / 3 + y] - 1] = false;
+			if (sdoku[1 + 3 * (c.x / 3) + x][1 + 3 * (c.y / 3) + y] != 0)
+				odds[sdoku[1 + 3 * (c.x / 3) + x][1 + 3 * (c.y / 3) + y] - 1] = false;
 		}
 	}
 }
 
-void dfs(int val)
+void DFS(int n)
 {
-	sdoku[v[val - 1].x][v[val - 1].y] = s.top();
-	s.pop();
+	//일단 pop 하고 그 숫자를 스도쿠에 넣는다.
+	pair<int, int> p = s.top();
 
-	checkPossibility(v[val]);
+	sdoku[vCoord[p.first].x][vCoord[p.first].y] = p.second;
 
-	for (int i = 0; i < 9; ++i)
+	if (layer == n)
 	{
-		if (odds[i])
+		if (check())
 		{
-			s.push(i + 1);
+			printSdoku();
+			done = true;
+			return;
+		}
+		else
+		{
+			return;
 		}
 	}
 
-	dfs(val + 1);
+	checkPossibility(n);
+
+	int personalOdds[9];
+
+	for (int i = 0; i < 9; ++i)
+	{
+		personalOdds[i] = odds[i];
+	}
+
+	for (int i = 0; i < 9; ++i)
+	{
+		if (personalOdds[i])
+		{
+			s.push(pair<int, int>{n, i + 1});
+			DFS(n + 1);
+			if (done)
+				return;
+			sdoku[vCoord[s.top().first].x][vCoord[s.top().first].y] = 0;
+			s.pop();
+		}
+	}
+}
+
+void doDFS()
+{
+	//0인 좌표들을 따냄
+	for (int i = 0; i < 9; ++i)
+	{
+		for (int j = 0; j < 9; ++j)
+		{
+			if (sdoku[i][j] == 0)
+				vCoord.push_back({ i, j });
+		}
+	}
+
+	layer = vCoord.size();
+
+	checkPossibility(0);
+
+	int personalOdds[9];
+
+	for (int i = 0; i < 9; ++i)
+	{
+		personalOdds[i] = odds[i];
+	}
+
+	for (int i = 0; i < 9; ++i)
+	{
+		if (personalOdds[i])
+		{
+			s.push(pair<int, int>{0, i + 1});
+			DFS(1);
+			if (done)
+				return;
+			sdoku[vCoord[s.top().first].x][vCoord[s.top().first].y] = 0;
+			s.pop();
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+
+	//입력 받는다
+	for (int i = 0; i < 9; ++i)
+	{
+		for (int j = 0; j < 9; ++j)
+		{
+			cin >> tmp;
+			sdoku[i][j] = tmp;
+		}
+	}
+
+	doDFS();
 }
